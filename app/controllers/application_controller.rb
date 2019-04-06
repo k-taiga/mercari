83e60135392ddf7aa8911a110154991659ee6cc0
Kobatai <taiga1983346@gmail.com>
2019-04-06 15:44:00 +0900

    deviseのconfigure_permitted_parametersの記述追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+
+private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:telephone,:email,:birth_year,:birth_month,:birth_day])
+  end
 
   def production?
     Rails.env.production?
   end
-
-  private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
