123f65ed7535356bb99fa95197c46cafdc69aea9
Kobatai <taiga1983346@gmail.com>
2019-04-06 15:48:22 +0900

    userモデルにvalidatesを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
 private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:telephone,:email,:birth_year,:birth_month,:birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:telephone,:birth_year,:birth_month,:birth_day])
   end
 
   def production?
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,:email,:telephone,:birth_year,:birth_month,:birth_day,presence: true
 end
